7b2fa8f9211f260275d3ea365a47995adae43019
KayRielz <azrilpribadi135@gmail.com>
2025-11-05 18:00:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -117,14 +117,29 @@ function getWeatherIcon(code) {
   const base = "https://www.tomorrow.io/static/img/icons/";
   const icons = {
     1000: "clear_day.svg",
+    1001: "cloudy.svg",
     1100: "mostly_clear_day.svg",
     1101: "partly_cloudy_day.svg",
-    1102: "cloudy.svg",
+    1102: "mostly_cloudy.svg",
+    2000: "fog.svg",
+    2100: "fog_light.svg",
+    4000: "drizzle.svg",
+    4001: "rain.svg",
     4200: "rain_light.svg",
     4201: "rain_heavy.svg",
-    2000: "fog.svg"
+    5000: "snow.svg",
+    5100: "snow_light.svg",
+    5101: "snow_heavy.svg",
+    6000: "freezing_rain_light.svg",
+    6001: "freezing_rain.svg",
+    6200: "sleet_light.svg",
+    6201: "sleet.svg",
+    7000: "ice_pellets.svg",
+    7101: "ice_pellets_heavy.svg",
+    7102: "ice_pellets_light.svg",
+    8000: "tstorm.svg"
   };
-  return base + (icons[code] || "unknown.svg");
+  return base + (icons[code] || "cloudy.svg");
 }
 
 // Saat pertama kali dibuka
